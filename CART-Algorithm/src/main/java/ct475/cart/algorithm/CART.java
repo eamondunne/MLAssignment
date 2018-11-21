800803aa42cd975d3eef49504b7035467d1a8a44
eamondunne <eamon.dunne@gmail.com>
2018-11-21 15:43:11 +0000

    Initial Class Format

--- CART-Algorithm/src/main/java/ct475/cart/algorithm/CART.java
+++ CART-Algorithm/src/main/java/ct475/cart/algorithm/CART.java
@@ -6,9 +6,56 @@
 package ct475.cart.algorithm;
 
 /**
- *
- * @author Cormac Buckley
+ * @author Cormac Buckley, Eamon Dunne
  */
 public class CART {
+//    Data Stuff Goes Here
+
+    /**
+     * Constructor
+     */
+    public CART() {
+
+    }
+
+    /**
+     * Calculate cost of split
+     */
+    void getSplitCost() {
+
+    }
+
+    /**
+    * Test costs for split
+    */
+    void testSplits() {
+
+    }
+
+    /**
+     * Split the tree
+     */
+    void splitTree() {
+
+    }
+    /**
+     * Build the tree
+     */
+    void buildTree() {
+        
+    }
     
+    /**
+     * Set Data
+     */
+    void setData() {
+
+    }
+    
+    /**
+     * Print Tree
+     */
+    void printTree() {
+        
+    }
 }
